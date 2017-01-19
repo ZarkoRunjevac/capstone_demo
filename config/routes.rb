81fd3d20d08dc77c20b8bc57cf0b940e061a5c27
Zarko Runjevac <zarko.runjevac@yahoo.com>
2017-01-19 13:24:15 +0100

    added main page with construction

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  root "ui#index"
+  get "/ui"=>'ui#index'
+  get "/ui#"=>'ui#index'
 
   scope :api, defaults: {format: :json} do
     resources :foos, except: [:new, :edit]
